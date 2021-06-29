e08d581edde0b5c2874918a947b226af9ca67964
Dominik <admin@dominik48n.de>
2021-06-29 09:50:41 +0200

    Reformat variables

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 if __name__ == "__main__":
     model.compile(optimizer='sgd', loss='mean_squared_error')
 
-    xs=[1, 2, 3]
-    ys=[2, 4, 6]
+    xs = [1, 2, 3]
+    ys = [2, 4, 6]
 
     model.fit(xs, ys, epochs=3000)
 
